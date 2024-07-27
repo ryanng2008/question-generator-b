79d258bbbb076a9fe1d5742c50e91a0e56b73a6e
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2024-07-28 00:25:42 +0800

    adding cors

--- application.py
+++ application.py
@@ -1,27 +1,31 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import algorithm.data_utils as du
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/home")
+@cross_origin()
 def home():
     return {"players": ["Stephen Curry", "Klay Thompson", "Andre Iguodala", "Draymond Green", "Andrew Bogut"]}
 
-@app.route("/questionlist/<categoryid>", defaults={'count': -1})
+@app.route("/questionlist/<categoryid>", defaults={'count': -1}, methods=['GET'])
 @app.route("/questionlist/<categoryid>/<count>")
+@cross_origin()
 def get_question_list(categoryid: str, count: int):
     return du.questions_from_cid(categoryid, int(count))
 
-@app.route("/categorydetails/<categoryid>")
+@app.route("/categorydetails/<categoryid>", methods=['GET'])
+@cross_origin()
 def get_category_details(categoryid: str):
     return du.get_category(categoryid)
 
-@app.route("/categorylist")
+@app.route("/categorylist", methods=['GET'])
+@cross_origin()
 def get_all_categories():
     return du.get_all_categories()
 
 if __name__ == "__main__":
     print('server ran')
     app.run(debug=True)
-
-    
